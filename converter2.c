129ffb38583f6f6f125cd60ceea65aa68f1b50d4
joannadebowska <54661186+joannadebowska@users.noreply.github.com>
2019-08-29 14:58:32 +0200

    Add files via upload

--- converter2.c
+++ converter2.c
@@ -3,18 +3,15 @@
 
 int main(int counter, char *words[])
 {
-	  for (int i = 1; words[i]!=NULL; ++i){ 
+	for (int i = 1; i<counter; ++i){ 
 		for (int j=0; words[i][j]!=0; ++j) {      	
 			if(isupper(words[i][j])){
 				words[i][j]= tolower(words[i][j]);
 				printf("_");
-			}
-			
+			}		
 			printf("%c", words[i][j]);
-	       	}
+		}	
 		printf("\n");
-       	}
-
-
+	}	
 return 0;
 }
